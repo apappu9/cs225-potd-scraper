0c412cf681b6839d0376fa1e2e37bad1cf2103be
Abhinav Pappu <abhi1pappu9@gmail.com>
2020-05-16 03:14:48 -0500

    Add login script

---
+++ login.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    userDataDir: path.resolve(__dirname, 'browser_data'), // saves cookies and other browser data in here (to preserve login)
+  });
+
+  const [page] = await browser.pages();
+
+  await page.goto('https://prairielearn.engr.illinois.edu/pl/');
+
+  const illinoisIconSelector = '.login-methods img[src="/images/illinois_logo.svg"]';
+  const logoutButtonSelector = 'a.btn[href="/pl/logout"]';
+  if (await page.$(illinoisIconSelector)) { // not logged in
+    await page.click(illinoisIconSelector);
+    console.log('Please login manually');
+
+    try {
+      // wait for the logout button to appear (indicates successful login)
+      await page.waitForSelector(logoutButtonSelector, { timeout: 10 * 60 * 1000 });
+      console.log("Successfully logged in! Please run 'node index.js' to scrape POTDs")
+    } catch {
+      console.log('Failed to login in time, please try again');
+    }
+  } else { // already logged in
+    console.log("You're already logged in. Please run 'node index.js' to scrape POTDs");
+  }
+
+  await browser.close();
+})()
